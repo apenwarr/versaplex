539391b56703e18d09f49f32ebd5977d1412459d
averyp <averyp@5b722feb-fb3a-0410-8233-6f4790b74332>
2007-08-08 01:48:36 +0000

    Mostly for test purposes, added an extra WvLog receiver that sends to any WvStream constructed from a moniker.  Seems to work fine with both SSL and plain tcp streams.  (Mostly, this is just an experiment to make sure WvSSLStream works right inside an ODBC driver dll.  Apparently so.)
    git-svn-id: http://versaplex.googlecode.com/svn/branches/middleware@74 5b722feb-fb3a-0410-8233-6f4790b74332

--- PacWest/src/Versaplex/vxodbc/wvlogger.cc
+++ PacWest/src/Versaplex/vxodbc/wvlogger.cc
@@ -2,16 +2,26 @@
 #include <wvwin32-sanitize.h>
 #include <wvlog.h>
 #include <wvlogfile.h>
+#include <wvlogstream.h>
+#include <wvmoniker.h>
+#include <wvlinkerhack.h>
+#include <wvistreamlist.h>
 
 static WvLog *log;
-static WvLogRcv *rcv1, *rcv2;
+static WvLogRcv *rcv1, *rcv2, *rcv3;
 
-extern char **_argv;
+WV_LINK_TO(WvTCPConn);
+WV_LINK_TO(WvSSLStream);
+
 
 void wvlog_open()
 {
     rcv1 = new WvLogConsole(dup(2));
     rcv2 = new WvLogFile("c:\\temp\\vxodbc.log");
+    IWvStream *s = wvcreate<IWvStream>("tcp:averyp-server:4444");
+    assert(s);
+    WvIStreamList::globallist.append(s, false, "tcp logger");
+    rcv3 = new WvLogStream(s, WvLog::Debug4);
     log = new WvLog(GetCurrentProcessId(), WvLog::Debug);
 }
 
@@ -20,18 +30,18 @@ void wvlog_print(const char *file, int line, const char *s)
 {
     if (!log)
 	wvlog_open();
+    while (WvIStreamList::globallist.select(0))
+	WvIStreamList::globallist.callback();
     log->print("%s:%s: %s", file, line, s);
 }
 
 
 void wvlog_close()
 {
-    if (log)
-	delete log;
-    if (rcv1)
-	delete rcv1;
-    if (rcv2)
-	delete rcv2;
+    if (log) delete log;
+    if (rcv1) delete rcv1;
+    if (rcv2) delete rcv2;
+    if (rcv3) delete rcv3;
     log = NULL;
-    rcv1 = rcv2 = NULL;
+    rcv1 = rcv2 = rcv3 = NULL;
 }
