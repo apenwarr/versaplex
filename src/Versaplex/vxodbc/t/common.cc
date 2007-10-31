09fddbb6408beb9b89246da195f4996862e13ac0
peter.mccurdy <peter.mccurdy@5b722feb-fb3a-0410-8233-6f4790b74332>
2007-10-31 22:42:48 +0000

    Start unit test work for VxODBC.  For starters, just one fairly simple test, with some provisions to make it easier to write more.
    git-svn-id: http://versaplex.googlecode.com/svn/trunk@151 5b722feb-fb3a-0410-8233-6f4790b74332

---
+++ src/Versaplex/vxodbc/t/common.cc
@@ -0,0 +1,343 @@
+#include "common.h"
+
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifndef WIN32
+#include "tds_sysdep_private.h"
+#else
+#define TDS_SDIR_SEPARATOR "\\"
+#endif
+
+HENV Environment;
+HDBC Connection;
+HSTMT Statement;
+int use_odbc_version3 = 0;
+
+char USER[512];
+char SERVER[512];
+char PASSWORD[512];
+char DATABASE[512];
+char DRIVER[1024];
+
+static int
+check_lib(char *path, const char *file)
+{
+	int len = strlen(path);
+	FILE *f;
+
+	strcat(path, file);
+	f = fopen(path, "rb");
+	if (f) {
+		fclose(f);
+		return 1;
+	}
+	path[len] = 0;
+	return 0;
+}
+
+/* some platforms do not have setenv, define a replacement */
+#if !HAVE_SETENV
+void
+odbc_setenv(const char *name, const char *value, int overwrite)
+{
+#if HAVE_PUTENV
+	char buf[1024];
+
+	sprintf(buf, "%s=%s", name, value);
+	putenv(buf);
+#endif
+}
+#endif
+
+int
+read_login_info(void)
+{
+	FILE *in;
+	char line[512];
+	char *s1, *s2;
+#ifndef WIN32
+	char path[1024];
+	int len;
+#endif
+
+	in = fopen(".." TDS_SDIR_SEPARATOR ".." TDS_SDIR_SEPARATOR ".." TDS_SDIR_SEPARATOR "PWD", "r");
+	if (!in)
+		in = fopen("PWD", "r");
+
+	if (!in) {
+		fprintf(stderr, "Can not open PWD file\n\n");
+                strcpy(USER, "pmccurdy");
+                strcpy(SERVER, "localhost");
+                strcpy(PASSWORD, "scs");
+                strcpy(DATABASE, "pmccurdy");
+		//return 0;
+        } else {
+            while (fgets(line, 512, in)) {
+                    s1 = strtok(line, "=");
+                    s2 = strtok(NULL, "\n");
+                    if (!s1 || !s2)
+                            continue;
+                    if (!strcmp(s1, "UID")) {
+                            strcpy(USER, s2);
+                    } else if (!strcmp(s1, "SRV")) {
+                            strcpy(SERVER, s2);
+                    } else if (!strcmp(s1, "PWD")) {
+                            strcpy(PASSWORD, s2);
+                    } else if (!strcmp(s1, "DB")) {
+                            strcpy(DATABASE, s2);
+                    }
+            }
+            fclose(in);
+        }
+
+#ifndef WIN32
+	/* find our driver */
+	if (!getcwd(path, sizeof(path)))
+		return 0;
+#if 0
+	len = strlen(path);
+	if (len < 10 || strcmp(path + len - 10, "/unittests") != 0)
+		return 0;
+	path[len - 9] = 0;
+	/* TODO this must be extended with all possible systems... */
+	if (!check_lib(path, ".libs/libtdsodbc.so") && !check_lib(path, ".libs/libtdsodbc.sl")
+	    && !check_lib(path, ".libs/libtdsodbc.dll") && !check_lib(path, ".libs/libtdsodbc.dylib"))
+		return 0;
+	strcpy(DRIVER, path);
+#endif
+
+	/* craft out odbc.ini, avoid to read wrong one */
+	in = fopen("odbc.ini", "w");
+	if (in) {
+		fprintf(in, "[%s]\nDriver = %s\nDatabase = %s\n"
+                        "Servername = %s\nUsername = %s\n", 
+                        SERVER, DRIVER, DATABASE, SERVER, USER);
+		fclose(in);
+		setenv("ODBCINI", "./odbc.ini", 1);
+		setenv("SYSODBCINI", "./odbc.ini", 1);
+	}
+#endif
+	return 0;
+}
+
+void
+ReportError(const char *errmsg, int line, const char *file)
+{
+	SQLSMALLINT handletype;
+	SQLHANDLE handle;
+	SQLRETURN ret;
+	unsigned char sqlstate[6];
+	unsigned char msg[256];
+
+
+	if (Statement) {
+		handletype = SQL_HANDLE_STMT;
+		handle = Statement;
+	} else if (Connection) {
+		handletype = SQL_HANDLE_DBC;
+		handle = Connection;
+	} else {
+		handletype = SQL_HANDLE_ENV;
+		handle = Environment;
+	}
+	if (errmsg[0]) {
+		if (line)
+			fprintf(stdout, "%s:%d %s\n", file, line, errmsg);
+		else
+			fprintf(stdout, "%s\n", errmsg);
+	}
+	ret = SQLGetDiagRec(handletype, handle, 1, sqlstate, NULL, msg, sizeof(msg), NULL);
+	if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO)
+		fprintf(stdout, "SQL error %s -- %s (%s:%d)\n", sqlstate, msg, file, line);
+}
+
+int
+Connect(void)
+{
+
+	int res;
+
+
+	char command[512];
+
+	if (read_login_info())
+		exit(1);
+
+	if (SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &Environment) != SQL_SUCCESS) {
+		printf("Unable to allocate env\n");
+		exit(1);
+	}
+
+	if (use_odbc_version3)
+		SQLSetEnvAttr(Environment, SQL_ATTR_ODBC_VERSION, (SQLPOINTER) (SQL_OV_ODBC3), SQL_IS_UINTEGER);
+
+	if (SQLAllocHandle(SQL_HANDLE_DBC, Environment, &Connection) != SQL_SUCCESS) {
+		printf("Unable to allocate connection\n");
+		SQLFreeHandle(SQL_HANDLE_ENV, Environment);
+		exit(1);
+	}
+	printf("odbctest\n--------\n\n");
+	printf("connection parameters:\nserver:   '%s'\nuser:     '%s'\npassword: '%s'\ndatabase: '%s'\n",
+	       SERVER, USER,  PASSWORD /* "????" */ , DATABASE);
+
+	res = SQLConnect(Connection, (SQLCHAR *) SERVER, SQL_NTS, (SQLCHAR *) USER, SQL_NTS, (SQLCHAR *) PASSWORD, SQL_NTS);
+	if (!SQL_SUCCEEDED(res)) {
+		printf("Unable to open data source (ret=%d)\n", res);
+		CheckReturn();
+		exit(1);
+	}
+
+	if (SQLAllocHandle(SQL_HANDLE_STMT, Connection, &Statement) != SQL_SUCCESS) {
+		printf("Unable to allocate statement\n");
+		CheckReturn();
+		exit(1);
+	}
+
+	sprintf(command, "use %s", DATABASE);
+	printf("%s\n", command);
+
+	if (!SQL_SUCCEEDED(SQLExecDirect(Statement, (SQLCHAR *) command, SQL_NTS))) {
+		printf("Unable to execute statement\n");
+		CheckReturn();
+		exit(1);
+	}
+	return 0;
+}
+
+int
+Disconnect(void)
+{
+	if (Statement) {
+		SQLFreeStmt(Statement, SQL_DROP);
+		Statement = SQL_NULL_HSTMT;
+	}
+
+	if (Connection) {
+		SQLDisconnect(Connection);
+		SQLFreeHandle(SQL_HANDLE_DBC, Connection);
+		Connection = SQL_NULL_HDBC;
+	}
+
+	if (Environment) {
+		SQLFreeHandle(SQL_HANDLE_ENV, Environment);
+		Environment = SQL_NULL_HENV;
+	}
+	return 0;
+}
+
+bool
+Command(HSTMT stmt, const char *command)
+{
+	int result = 0;
+
+	fprintf(stderr, "%s\n", command);
+	result = SQLExecDirect(stmt, (SQLCHAR *) command, SQL_NTS);
+	if (result != SQL_SUCCESS && result != SQL_NO_DATA) {
+		fprintf(stderr, "Unable to execute statement\n");
+		CheckReturn();
+		return false;
+	}
+        return true;
+}
+
+SQLRETURN
+CommandWithResult(HSTMT stmt, const char *command)
+{
+	printf("%s\n", command);
+	return SQLExecDirect(stmt, (SQLCHAR *) command, SQL_NTS);
+}
+
+static int ms_db = -1;
+int
+db_is_microsoft(void)
+{
+	char buf[64];
+	SQLSMALLINT len;
+	int i;
+
+	if (ms_db < 0) {
+		buf[0] = 0;
+		SQLGetInfo(Connection, SQL_DBMS_NAME, buf, sizeof(buf), &len);
+		for (i = 0; buf[i]; ++i)
+			buf[i] = tolower(buf[i]);
+		ms_db = (strstr(buf, "microsoft") != NULL);
+	}
+	return ms_db;
+}
+
+static int freetds_driver = -1;
+int
+driver_is_freetds(void)
+{
+	char buf[64];
+	SQLSMALLINT len;
+	int i;
+
+	if (freetds_driver < 0) {
+		buf[0] = 0;
+		SQLGetInfo(Connection, SQL_DRIVER_NAME, buf, sizeof(buf), &len);
+		for (i = 0; buf[i]; ++i)
+			buf[i] = tolower(buf[i]);
+		freetds_driver = (strstr(buf, "tds") != NULL);
+	}
+	return freetds_driver;
+}
+
+void
+CheckCols(int n, int line, const char * file)
+{
+	SQLSMALLINT cols;
+	SQLRETURN res;
+
+	res = SQLNumResultCols(Statement, &cols);
+	if (res != SQL_SUCCESS) {
+		if (res == SQL_ERROR && n < 0)
+			return;
+		fprintf(stderr, "%s:%d: Unable to get column numbers\n", file, line);
+		CheckReturn();
+		Disconnect();
+		exit(1);
+	}
+
+	if (cols != n) {
+		fprintf(stderr, "%s:%d: Expected %d columns returned %d\n", file, line, n, (int) cols);
+		Disconnect();
+		exit(1);
+	}
+}
+
+void
+CheckRows(int n, int line, const char * file)
+{
+	SQLLEN rows;
+	SQLRETURN res;
+
+	res = SQLRowCount(Statement, &rows);
+	if (res != SQL_SUCCESS) {
+		if (res == SQL_ERROR && n < -1)
+			return;
+		fprintf(stderr, "%s:%d: Unable to get row\n", file, line);
+		CheckReturn();
+		Disconnect();
+		exit(1);
+	}
+
+	if (rows != n) {
+		fprintf(stderr, "%s:%d: Expected %d rows returned %d\n", file, line, n, (int) rows);
+		Disconnect();
+		exit(1);
+	}
+}
+
+void
+ResetStatement(void)
+{
+	SQLFreeStmt(Statement, SQL_DROP);
+	Statement = SQL_NULL_HSTMT;
+	if (SQLAllocHandle(SQL_HANDLE_STMT, Connection, &Statement) != SQL_SUCCESS)
+		ODBC_REPORT_ERROR("Unable to allocate statement");
+}
+
---
+++ src/Versaplex/vxodbc/t/t0001.cc
@@ -0,0 +1,413 @@
+#include "wvstring.h"
+#include "wvdbusserver.h"
+#include "wvdbusconn.h"
+#include "wvtest.h"
+#include "fileutils.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "common.h"
+
+#include <vector>
+
+// FIXME: This needs a namespace prefix
+enum ColumnType {
+    Int64 = 0,
+    Int32,
+    Int16,
+    UInt8,
+    Bool,
+    Double,
+    Uuid,
+    Binary,
+    String,
+    DateTime,
+    Decimal,
+    ColumnTypeMax
+};
+
+WvString gColTypeNames[ColumnTypeMax + 1] = {
+    "Int64",
+    "Int32",
+    "Int16",
+    "UInt8",
+    "Bool",
+    "Double",
+    "Uuid",
+    "Binary",
+    "String",
+    "DateTime",
+    "Decimal",
+    WvString::null
+};
+
+class ColumnInfo
+{
+public:
+    int size;
+    WvString colname;
+    ColumnType coltype;
+    short precision;
+    short scale;
+    unsigned char nullable;
+
+    ColumnInfo(WvString _colname, ColumnType _coltype, bool _nullable,
+            int _size, short _precision, short _scale) :
+        size(_size),
+        colname(_colname), 
+        coltype(_coltype), 
+        precision(_precision),
+        scale(_scale),
+        nullable(_nullable)
+    {
+    }
+
+    void writeHeader(WvDBusMsg &msg)
+    {
+        msg.struct_start("issnny");
+        msg.append(size);
+        msg.append(colname);
+        msg.append(coltype);
+        msg.append(precision);
+        msg.append(scale);
+        // FIXME: This cast is due to a silly bug in WvStreams, append()
+        // should have an overload for an unsigned char
+        msg.append((signed char)nullable);
+        msg.struct_end();
+    }
+
+    // Note: These use the MSSQL names for data types.
+    // FIXME: It may make sense to specify the SQL datatypes in the enum, and
+    // convert them to more DBussy types later, IOW flip this around.
+    WvString getSqlColtype()
+    {
+        switch(coltype) {
+        case Int64:
+            return "bigint";
+        case Int32:
+            return "int";
+        case Int16:
+            return "smallint";
+        case UInt8:
+            return "tinyint";
+        case Bool:
+            return "bit";
+        case Double:
+            return "float";
+        case Uuid:
+            return "uniqueidentifier";
+        case Binary:
+            return "binary";
+        case String:
+            if (size > 0)
+                return WvString("varchar(%s)", size);
+            else
+                return WvString("text");
+        case DateTime:
+            return "datetime";
+        case Decimal:
+            return WvString("numeric(%s,%s)", precision, scale);
+        case ColumnTypeMax:
+        default:
+            WVFAILEQ(WvString("Unknown SQL type %d", coltype), WvString::null);
+            return "Unknown";
+        }
+    }
+};
+
+class Column
+{
+public:
+    ColumnInfo info;
+    // A bunch of malloc'd data.  Cast it back to whatever is appropriate.
+    std::vector<void *> data;
+
+    Column(ColumnInfo _info) : info(_info)
+    {
+    }
+
+    ~Column()
+    {
+        std::vector<void *>::iterator it;
+        for (it = data.begin(); it != data.end(); ++it)
+        {
+            free(*it);
+        }
+    }
+};
+
+class Table
+{
+public:
+    std::vector<Column> cols;
+    WvString name;
+
+    Table(WvStringParm _name) : name(_name)
+    {
+    }
+
+    ~Table()
+    {
+    }
+
+    void addCol(WvStringParm colname, ColumnType coltype, bool nullable,
+            int size, short precision, short scale)
+    {
+        cols.push_back(
+            Column(ColumnInfo(colname, coltype, nullable, size, precision, scale)));
+    }
+
+    // String lenth of 0 means use unlimited length (TEXT as opposed to
+    // VARCHAR(N)
+    void addStringCol(WvStringParm colname, int size, bool nullable)
+    {
+        addCol(colname, String, nullable, size, 0, 0);
+    }
+
+    WvString getCreateTableStmt()
+    {
+        WvString result("create table %s (", name);
+
+        std::vector<Column>::iterator it;
+        for (it = cols.begin(); it != cols.end(); ++it)
+        {
+            if (it != cols.begin()) 
+                result.append(",");
+
+            Column &col = *it;
+            result.append("%s %s%s", 
+                col.info.colname,
+                col.info.getSqlColtype(),
+                col.info.nullable ? "" : " not null");
+        }
+        result.append(")");
+        return result;
+    }
+};
+
+class TestDBusServer
+{
+public:
+    WvString moniker;
+    WvDBusServer *s;
+
+    TestDBusServer()
+    {
+        fprintf(stderr, "Creating a test DBus server.\n");
+        WvString smoniker("unix:tmpdir=%s.dir", wvtmpfilename("wvdbus-sock-"));
+        s = new WvDBusServer(smoniker);
+        moniker = s->get_addr();
+        fprintf(stderr, "Server address is '%s'\n", moniker.cstr());
+        WvIStreamList::globallist.append(s, false);
+    }
+
+    ~TestDBusServer()
+    {
+        delete s;
+    }
+};
+
+void add_colinfo(WvDBusMsg &msg, int size, WvStringParm colname, 
+    WvStringParm coltype, short precision, short scale, unsigned char nullable)
+{
+    msg.struct_start("issnny");
+    msg.append(size);
+    msg.append(colname);
+    msg.append(coltype);
+    msg.append(precision);
+    msg.append(scale);
+    msg.append((signed char)nullable);
+    msg.struct_end();
+}
+
+class FakeVersaplexServer
+{
+public:
+    TestDBusServer dbus;
+    WvDBusConn vxserver_conn;
+    Table *t;
+
+    // FIXME: Use a private bus when we can tell VxODBC where to find it.
+    // Until then, just use the session bus and impersonate Versaplex, 
+    // hoping that no other Versaplex server is running.
+    FakeVersaplexServer() : vxserver_conn("dbus:session"),
+        t(NULL)
+    {
+        WvIStreamList::globallist.append(&vxserver_conn, false);
+
+        fprintf(stderr, "*** Registering com.versabanq.versaplex\n");
+        vxserver_conn.request_name("com.versabanq.versaplex", &name_request_cb);
+        while (num_names_registered < 1)
+            WvIStreamList::globallist.runonce();
+
+        WvDBusCallback cb(this, &FakeVersaplexServer::msg_received);
+        vxserver_conn.add_callback(WvDBusConn::PriNormal, cb);
+    }
+
+    static int num_names_registered;
+    static bool name_request_cb(WvDBusConn &conn, WvDBusMsg &msg) 
+    {
+        num_names_registered++;
+        // FIXME: Sensible logging
+        // FIXME: Do something useful if the name was already registered
+        fprintf(stderr, "*** A name was registered: %s\n", ((WvString)msg).cstr());
+        return true;
+    }
+
+    bool msg_received(WvDBusConn &conn, WvDBusMsg &msg) 
+    {
+        if (msg.get_dest() != "com.versabanq.versaplex")
+            return false;
+
+        if (msg.get_path() != "/com/versabanq/versaplex/db") 
+            return false;
+
+        if (msg.get_interface() != "com.versabanq.versaplex.db") 
+            return false;
+
+        // The message was for us
+
+        printf("sender:%s\ndest:%s\npath:%s\niface:%s\nmember:%s\n",
+            msg.get_sender().cstr(), msg.get_dest().cstr(), msg.get_path().cstr(), 
+            msg.get_interface().cstr(), msg.get_member().cstr());
+
+        fprintf(stdout, "*** Received message %s\n", ((WvString)msg).cstr());
+        printf("*** Got argstr '%s'\n", msg.get_argstr().cstr());
+
+#if 0
+        WvDBusMsg::Iter ii(msg);
+        for (ii.rewind(); ii.next(); )
+        {
+            printf("**** Contents: %s\n", ii
+        }
+#endif
+
+
+        if (msg.get_member() == "ExecRecordset")
+        {
+            WvString query(msg.get_argstr());
+            if (query == "use pmccurdy")
+            {
+                printf("*** Sending error\n");
+                WvDBusError(msg, "System.ArgumentOutOfRangeException", 
+                    "Argument is out of range.").send(conn);
+                return false;
+            }
+            else if (query == "select * from odbctestdata")
+            {
+                printf("*** Sending reply\n");
+                WvDBusMsg reply = msg.reply();
+                reply.array_start("(issnny)");
+#if 1
+                add_colinfo(reply, 30, "col1", "String", 0, 0, 0);
+                add_colinfo(reply, 4, "col2", "Int32", 0, 0, 0);
+                add_colinfo(reply, 8, "col3", "Double", 0, 0, 0);
+                add_colinfo(reply, 17, "col4", "Decimal", 0, 0, 0);
+                add_colinfo(reply, 8, "col5", "DateTime", 0, 0, 0);
+                add_colinfo(reply, INT_MAX, "col6", "String", 0, 0, 0);
+#else
+                std::vector<Column>::iterator it;
+                for (it = t->cols.begin(); it != t->cols.end(); ++it)
+                    it->info.writeHeader(reply);
+#endif
+                reply.array_end();
+                reply.varray_start("(sids(ii)s)");
+                reply.struct_start("sids(ii)s");
+                reply.append("ABCDEFGHIJKLMNOP");
+                reply.append(123456);
+                reply.append(1234.0);
+                reply.append("123456.780000");
+                reply.struct_start("ii");
+                reply.append(1000202400);
+                reply.append(0);
+                reply.struct_end();
+                reply.append("just to check returned length...");
+                reply.struct_end();
+                reply.varray_end();
+                reply.array_start("ay");
+                reply.array_start("y");
+                // Nullity
+                reply.append((char)0);
+                reply.append((char)0);
+                reply.append((char)0);
+                reply.append((char)0);
+                reply.append((char)0);
+                reply.append((char)0);
+                reply.array_end();
+                reply.array_end();
+                reply.send(conn);
+            }
+            else
+            {
+                WvDBusError(msg, "System.NotImplemented", 
+                    "Not yet implemented.  Try again later.").send(conn);
+            }
+            //msg.reply().append(WvString("baz")).send(conn);
+        }
+        return true;
+    }
+};
+
+int FakeVersaplexServer::num_names_registered = 0;
+
+// FIXME: Move this to a header, and use a more sensible error checking
+// function
+#define WVPASS_SQL(sql) \
+    do \
+    { \
+        if (!WvTest::start_check(__FILE__, __LINE__, #sql, SQL_SUCCEEDED(sql)))\
+            ReportError(#sql, __LINE__, __FILE__); \
+    } while (0)
+#define WVPASS_SQL_EQ(x, y) do { if (!WVPASSEQ((x), (y))) { CheckReturn(); } } while (0)
+
+int main(int argc, char *argv[])
+{
+    FakeVersaplexServer v;
+    WvString command;
+
+    Connect();
+
+    WVPASS_SQL(CommandWithResult(Statement, "drop table odbctestdata"));
+
+    bool not_null = 0;
+    Table t("odbctestdata");
+    t.addCol("col1", String, not_null, 30, 0, 0);
+    t.addCol("col2", Int32, not_null, 4, 0, 0);
+    t.addCol("col3", Double, not_null, 8, 0, 0);
+    t.addCol("col4", Decimal, not_null, 0, 18, 6);
+    t.addCol("col5", DateTime, not_null, 8, 0, 0);
+    t.addCol("col6", String, not_null, 0, 0, 0);
+    v.t = &t;
+
+    WVPASS_SQL(CommandWithResult(Statement, t.getCreateTableStmt()));
+
+    command = "insert dbo.odbctestdata values ("
+        "'ABCDEFGHIJKLMNOP',"
+        "123456," "1234.56," "123456.78," "'Sep 11 2001 10:00AM'," 
+        "'just to check returned length...')";
+    WVPASS_SQL(CommandWithResult(Statement, command)); 
+
+    WVPASS_SQL(CommandWithResult(Statement, "select * from odbctestdata"));
+
+    WVPASS_SQL(SQLFetch(Statement));
+
+    for (int i = 1; i <= 6; i++) {
+        SQLLEN cnamesize;
+        SQLCHAR output[256];
+
+        WVPASS_SQL(SQLGetData(Statement, i, SQL_C_CHAR, 
+                                output, sizeof(output), &cnamesize));
+        WVPASSEQ((int)cnamesize, strlen((char *)output));
+    }
+
+    WVPASS_SQL_EQ(SQLFetch(Statement), SQL_NO_DATA);
+    WVPASS_SQL(SQLCloseCursor(Statement));
+
+    WVPASS_SQL(CommandWithResult(Statement, "drop table odbctestdata"));
+
+    Disconnect();
+
+    printf("Done.\n");
+    return 0;
+}
