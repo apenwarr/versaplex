13e929679c89077d3290fe0aaaf1f973cf0bd6a8
apenwarr <apenwarr@5b722feb-fb3a-0410-8233-6f4790b74332>
2007-10-31 21:56:08 +0000

    Switch from the wvstreams dbus-1 tag to the wvstreams-ng branch.  I kind of expected that would be... work.  Apparently not.  Well, that's nice.
    git-svn-id: http://versaplex.googlecode.com/svn/trunk@149 5b722feb-fb3a-0410-8233-6f4790b74332

--- src/Versaplex/vxodbc/wvlogger.cc
+++ src/Versaplex/vxodbc/wvlogger.cc
@@ -8,7 +8,7 @@
 #include <wvcrash.h>
 #include "psqlodbc.h"
 
-static WvLog *log;
+static WvLog *wvlog;
 static WvLogRcv *rcv1, *rcv2, *rcv3;
 
 WV_LINK_TO(WvTCPConn);
@@ -28,27 +28,27 @@ void wvlog_open()
     assert(s);
     WvIStreamList::globallist.append(s, false, "tcp logger");
     rcv3 = new WvLogStream(s, WvLog::Debug4);
-    log = new WvLog(getpid(), WvLog::Debug);
+    wvlog = new WvLog(getpid(), WvLog::Debug);
 }
 
 
 void wvlog_print(const char *file, int line, const char *s)
 {
-    if (!log)
+    if (!wvlog)
 	wvlog_open();
     while (WvIStreamList::globallist.select(0))
 	WvIStreamList::globallist.callback();
     WvString ss("%s:%s: %s", file, line, s);
-    log->print(ss);
+    wvlog->print(ss);
 }
 
 
 void wvlog_close()
 {
-    if (log) delete log;
+    if (wvlog) delete wvlog;
     if (rcv1) delete rcv1;
     if (rcv2) delete rcv2;
     if (rcv3) delete rcv3;
-    log = NULL;
+    wvlog = NULL;
     rcv1 = rcv2 = rcv3 = NULL;
 }
