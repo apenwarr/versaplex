cde1233a6b0d21838eacc6261a728a65d48afdfc
peter.mccurdy <peter.mccurdy@5b722feb-fb3a-0410-8233-6f4790b74332>
2007-11-08 00:53:31 +0000

    Add another FreeTDS unit test, this one checking that destroying one ODBC connection doesn't touch pending data on another connection.
    git-svn-id: http://versaplex.googlecode.com/svn/trunk@180 5b722feb-fb3a-0410-8233-6f4790b74332

---
+++ vxodbc/t/t0002.t.cc
@@ -0,0 +1,53 @@
+#include "common.h"
+#include "wvtest.h"
+#include "table.h"
+
+#include "fakeversaplex.h"
+
+WVTEST_MAIN("Dropped statements don't destroy pending data")
+{
+    FakeVersaplexServer v;
+    Table t("odbctestdata");
+    bool null = 1;
+    t.addCol("i", ColumnInfo::Int32, null, 4, 0, 0);
+    v.t = &t;
+
+    Connect();
+
+    WVPASS_SQL(CommandWithResult(Statement, "drop table odbctestdata"));
+    WVPASS_SQL(CommandWithResult(Statement, "create table odbctestdata (i int)"));
+    WVPASS_SQL(CommandWithResult(Statement, "insert odbctestdata values (123)"));
+
+    /*
+     * now we allocate another statement, select, get all results
+     * then make another query with first select and drop this statement
+     * result should not disappear (required for DBD::ODBC)
+     */
+
+    HSTMT stmt;
+    WvString select = "select * from odbctestdata";
+    WvString empty_select = "select * from odbctestdata where 0=1";
+    v.expected_query = empty_select;
+    WVPASS_SQL(SQLAllocHandle(SQL_HANDLE_STMT, Connection, &stmt));
+    WVPASS_SQL(CommandWithResult(stmt, empty_select.cstr()));
+    WVPASS_SQL_EQ(SQLFetch(stmt), SQL_NO_DATA);
+    WVPASS_SQL(SQLCloseCursor(stmt));
+
+    v.expected_query = select;
+    v.t->cols[0].append(123);
+    WVPASS_SQL(CommandWithResult(Statement, select.cstr()));
+
+    /* drop first statement .. data should not disappear */
+    WVPASS_SQL(SQLFreeStmt(stmt, SQL_DROP));
+
+    long value = 0;
+    WVPASS_SQL(SQLFetch(Statement));
+    WVPASS_SQL(SQLGetData(Statement, 1, SQL_C_LONG, &value, 0, NULL));
+    WVPASSEQ(value, 123);
+    WVPASS_SQL_EQ(SQLFetch(Statement), SQL_NO_DATA);
+
+    WVPASS_SQL(SQLCloseCursor(Statement));
+    WVPASS_SQL(CommandWithResult(Statement, "drop table odbctestdata"));
+
+    Disconnect();
+}
