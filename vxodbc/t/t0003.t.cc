f9011fa42c7f18506fa2035e1806b12cbfc5cbd1
peter.mccurdy <peter.mccurdy@5b722feb-fb3a-0410-8233-6f4790b74332>
2007-11-08 00:47:22 +0000

    Use a table name the real Versaplex can handle, and tell the fake Versaplex what kind of data to use.  Still doesn't work.
    git-svn-id: http://versaplex.googlecode.com/svn/trunk@179 5b722feb-fb3a-0410-8233-6f4790b74332

--- vxodbc/t/t0003.t.cc
+++ vxodbc/t/t0003.t.cc
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "fakeversaplex.h"
+#include "table.h"
 #include "wvtest.h"
 
 /* Test for SQLMoreResults */
@@ -8,35 +9,39 @@ static void
 DoTest(int prepared)
 {
     FakeVersaplexServer v;
-    Command(Statement, "create table #odbctestdata (i int)");
+    Table t("odbctestdata");
+    bool null = 1;
+    t.addCol("i", ColumnInfo::Int32, null, 4, 0, 0);
+    v.t = &t;
+    Command(Statement, "create table odbctestdata (i int)");
 
     /* test that 2 empty result set are returned correctly */
+    v.expected_query =
+        "select * from odbctestdata select * from odbctestdata"; 
     if (!prepared) {
-        WVPASS_SQL(Command(Statement, 
-                    "select * from #odbctestdata select * from #odbctestdata"));
+        WVPASS_SQL(Command(Statement, v.expected_query.cstr()));
     } else {
-        WVPASS_SQL(SQLPrepare(Statement, (SQLCHAR *)
-                    "select * from #odbctestdata select * from #odbctestdata", 
-                    SQL_NTS));
+        WVPASS_SQL(SQLPrepare(Statement, (SQLCHAR *)v.expected_query.cstr(), 
+            SQL_NTS));
         WVPASS_SQL(SQLExecute(Statement));
     }
 
     WVPASS_SQL_EQ(SQLFetch(Statement), SQL_NO_DATA);
-
     WVPASS_SQL(SQLMoreResults(Statement));
+
     printf("Getting next recordset\n");
 
     WVPASS_SQL_EQ(SQLFetch(Statement), SQL_NO_DATA);
     WVPASS_SQL_EQ(SQLMoreResults(Statement), SQL_NO_DATA);
 
     /* test that skipping a no empty result go to other result set */
-    WVPASS_SQL(Command(Statement, "insert into #odbctestdata values(123)"));
+    WVPASS_SQL(Command(Statement, "insert into odbctestdata values(123)"));
     if (!prepared) {
         WVPASS_SQL(Command(Statement, 
-            "select * from #odbctestdata select * from #odbctestdata"));
+            "select * from odbctestdata select * from odbctestdata"));
     } else {
         WVPASS_SQL(SQLPrepare(Statement, (SQLCHAR *)
-            "select * from #odbctestdata select * from #odbctestdata", SQL_NTS));
+            "select * from odbctestdata select * from odbctestdata", SQL_NTS));
         WVPASS_SQL(SQLExecute(Statement));
     }
 
@@ -48,17 +53,19 @@ DoTest(int prepared)
     WVPASS_SQL_EQ(SQLFetch(Statement), SQL_NO_DATA);
     WVPASS_SQL_EQ(SQLMoreResults(Statement), SQL_NO_DATA);
 
-    WVPASS_SQL(Command(Statement, "drop table #odbctestdata"));
+    WVPASS_SQL(Command(Statement, "drop table odbctestdata"));
 }
 
-WVTEST_MAIN("Two empty record sets")
+WVTEST_MAIN("Two empty record sets, unprepared")
 {
     Connect();
-
     DoTest(0);
-    DoTest(1);
-
     Disconnect();
+}
 
-    printf("Done.\n");
+WVTEST_MAIN("Two empty record sets, prepared")
+{
+    Connect();
+    DoTest(0);
+    Disconnect();
 }
