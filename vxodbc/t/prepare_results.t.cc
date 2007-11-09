b62ae8efd393b0b653eba9fc51ad7d665733e0fd
peter.mccurdy <peter.mccurdy@5b722feb-fb3a-0410-8233-6f4790b74332>
2007-11-09 03:17:04 +0000

    Add another unit test, running through SQLPrepare.  Since VxODBC doesn't really support SQLPrepare properly, it doesn't yet test much.  See GoogleCode bugs #3 and #4.
    git-svn-id: http://versaplex.googlecode.com/svn/trunk@184 5b722feb-fb3a-0410-8233-6f4790b74332

---
+++ vxodbc/t/prepare_results.t.cc
@@ -0,0 +1,91 @@
+#include "common.h"
+#include "wvtest.h"
+#include "fakeversaplex.h"
+#include "table.h"
+
+/* Test for data format returned from SQLPrepare */
+
+WVTEST_MAIN("SQLPrepare results")
+{
+    FakeVersaplexServer v;
+    Table t("odbctestdata");
+    bool null = 1;
+    t.addCol("i", ColumnInfo::Int32, null, 4, 0, 0);
+    t.addCol("c", ColumnInfo::String, null, 20, 0, 0);
+    t.addCol("n", ColumnInfo::Decimal, null, 17, 34, 12);
+    v.t = &t;
+
+    Connect();
+
+    v.expected_query = 
+        "create table odbctestdata (i int, c char(20), n numeric(34,12) )";
+    WVPASS_SQL(CommandWithResult(Statement, v.expected_query));
+
+    /* reset state */
+    v.expected_query = "select * from odbctestdata";
+    WVPASS_SQL(CommandWithResult(Statement, v.expected_query.cstr()));
+    SQLFetch(Statement);
+    SQLMoreResults(Statement);
+
+    /* test query returns column information for update */
+    v.expected_query = "update odbctestdata set i = 20";
+#ifdef VXODBC_SUPPORTS_SQLPREPARE
+    WVPASS_SQL(SQLPrepare(Statement, (SQLCHAR *)v.expected_query.cstr(), SQL_NTS));
+#else
+    WVPASS_SQL(CommandWithResult(Statement, v.expected_query.cstr()));
+#endif
+
+    SQLSMALLINT count = 0; 
+#ifdef VXODBC_SUPPORTS_EMPTY_RESULTSETS
+    WVPASS_SQL(SQLNumResultCols(Statement, &count));
+    WVPASSEQ(count, 0);
+#endif
+
+    /* test query returns column information */
+    v.expected_query = "select * from odbctestdata";
+#ifdef VXODBC_SUPPORTS_SQLPREPARE
+    WVPASS_SQL(SQLPrepare(Statement, (SQLCHAR *)v.expected_query.cstr(), SQL_NTS));
+#else
+    WVPASS_SQL(CommandWithResult(Statement, v.expected_query.cstr()));
+#endif
+
+    WVPASS_SQL(SQLNumResultCols(Statement, &count));
+    WVPASSEQ(count, 3);
+
+#ifdef VXODBC_SUPPORTS_SQLDESCRIBECOL
+    SQLSMALLINT namelen = 0, type = 0, digits = 0, nullable = 0;
+    SQLULEN size = 0;
+    char name[128] = { 0 };
+
+    WVPASS_SQL(SQLDescribeCol(Statement, 1, (SQLCHAR *) name, sizeof(name), 
+            &namelen, &type, &size, &digits, &nullable));
+
+    WVPASSEQ(type, SQL_INTEGER);
+    WVPASSEQ(name, "i");
+
+    WVPASS_SQL(SQLDescribeCol(Statement, 2, (SQLCHAR *) name, sizeof(name), 
+            &namelen, &type, &size, &digits, &nullable));
+
+    WVPASSEQ(type, SQL_CHAR);
+    WVPASSEQ(name, "c"); 
+    WVPASSEQ(size, 20);
+
+    WVPASS_SQL(SQLDescribeCol(Statement, 3, (SQLCHAR *) name, sizeof(name), 
+            &namelen, &type, &size, &digits, &nullable));
+
+    WVPASSEQ(type, SQL_NUMERIC);
+    WVPASSEQ(name, "n"); 
+    WVPASSEQ(size, 34); 
+    WVPASSEQ(digits, 12);
+#endif
+
+    /* TODO test SQLDescribeParam (when implemented) */
+
+    // Disable so it's easier to run against the real Versaplex (which doesn't 
+    // support CREATE TABLE just yet).
+#if VERSAPLEX_SUPPORTS_CREATE_TABLE
+    Command(Statement, "drop table odbctestdata");
+#endif
+
+    Disconnect();
+}
