0daa01d2a3ba92301e462d8183376f94d3fd78d5
peter.mccurdy <peter.mccurdy@5b722feb-fb3a-0410-8233-6f4790b74332>
2007-11-09 20:35:41 +0000

    Add a unit test that looks at SQLGetData.
    git-svn-id: http://versaplex.googlecode.com/svn/trunk@185 5b722feb-fb3a-0410-8233-6f4790b74332

---
+++ vxodbc/t/getdata.t.cc
@@ -0,0 +1,38 @@
+#include "common.h"
+#include "wvtest.h"
+#include "table.h"
+#include "fakeversaplex.h"
+
+WVTEST_MAIN("SQLGetData")
+{
+    FakeVersaplexServer v;
+    Table t("whatever");
+    t.addCol("", ColumnInfo::String, 1, 0, 0, 0);
+    t.cols[0].append("Prova");
+    v.t = &t;
+    char buf[16];
+
+    Connect();
+
+    /* TODO test with VARCHAR too */
+    v.expected_query = "SELECT CONVERT(TEXT,'Prova')";
+    WVPASS_SQL(Command(Statement, v.expected_query.cstr()));
+
+    WVPASS_SQL(SQLFetch(Statement));
+
+    /* these 2 tests test an old severe BUG in FreeTDS */
+    WVPASS_SQL_EQ(SQLGetData(Statement, 1, SQL_C_CHAR, buf, 0, NULL), 
+            SQL_SUCCESS_WITH_INFO);
+
+    WVPASS_SQL_EQ(SQLGetData(Statement, 1, SQL_C_CHAR, buf, 0, NULL), 
+            SQL_SUCCESS_WITH_INFO);
+
+    WVPASS_SQL_EQ(SQLGetData(Statement, 1, SQL_C_CHAR, buf, 3, NULL), 
+            SQL_SUCCESS_WITH_INFO);
+    WVPASSEQ(buf, "Pr");
+
+    WVPASS_SQL(SQLGetData(Statement, 1, SQL_C_CHAR, buf, 16, NULL));
+    WVPASSEQ(buf, "ova");
+
+    Disconnect();
+}
