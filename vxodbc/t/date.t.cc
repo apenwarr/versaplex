2832da733b8c6b77d6f9f133fbe396bdfd16258f
peter.mccurdy <peter.mccurdy@5b722feb-fb3a-0410-8233-6f4790b74332>
2007-11-13 07:06:35 +0000

    Add a unit test for retrieving values from a DateTime column.  It doesn't work either.  See GoogleCode bug #6.
    git-svn-id: http://versaplex.googlecode.com/svn/trunk@187 5b722feb-fb3a-0410-8233-6f4790b74332

---
+++ vxodbc/t/date.t.cc
@@ -0,0 +1,68 @@
+#include "common.h"
+#include "fakeversaplex.h"
+#include "table.h"
+#include "wvtest.h"
+
+static void DoTest(int convert_to_char)
+{
+    FakeVersaplexServer v;
+    Table t("unnamed");
+    t.addCol("unnamed", ColumnInfo::DateTime, 0, 8, 0, 0);
+    // time_t value for 2002-12-27 18:43:21
+    t.cols[0].append(1041014601).append(0);
+    v.t = &t;
+    int res;
+
+    SQLCHAR output[256];
+
+    SQLLEN dataSize;
+
+    TIMESTAMP_STRUCT ts;
+
+    v.expected_query = "select convert(datetime, '2002-12-27 18:43:21')";
+    WVPASS_SQL(CommandWithResult(Statement, v.expected_query));
+
+    WVPASS_SQL(SQLFetch(Statement));
+
+#ifdef VXODBC_SUPPORTS_SQLDESCRIBECOL
+    SQLSMALLINT colType;
+    SQLULEN colSize;
+    SQLSMALLINT colScale, colNullable;
+    WVPASS_SQL(SQLDescribeCol(Statement, 1, output, sizeof(output), NULL, 
+            &colType, &colSize, &colScale, &colNullable));
+#endif
+
+    if (convert_to_char == 0) {
+        memset(&ts, 0, sizeof(ts));
+        WVPASS_SQL(SQLGetData(Statement, 1, SQL_C_TIMESTAMP, &ts, sizeof(ts), 
+            &dataSize));
+        sprintf((char *) output, "%04d-%02d-%02d %02d:%02d:%02d.000", 
+            ts.year, ts.month, ts.day, ts.hour, ts.minute, ts.second);
+    } else {
+        WVPASS_SQL(SQLGetData(Statement, 1, SQL_C_CHAR, output, sizeof(output), &dataSize));
+    }
+
+#ifdef VXODBC_SUPPORTS_DATETIME_RETRIEVAL
+    WVPASSEQ((char *) output, "2002-12-27 18:43:21.000");
+#else
+    printf("Date returned: %s\n", output);
+#endif
+
+    WVPASS_SQL_EQ(SQLFetch(Statement), SQL_NO_DATA);
+
+    WVPASS_SQL(SQLCloseCursor(Statement));
+}
+
+WVTEST_MAIN("Date conversion to timestamp")
+{
+    Connect();
+    DoTest(0);
+    Disconnect();
+}
+
+WVTEST_MAIN("Date conversion to char")
+{
+    Connect();
+    DoTest(1);
+    Disconnect();
+}
