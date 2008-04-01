0b34d2967ec435cb72309d73fa4fbe51628adeaa
Avery Pennarun <apenwarr@versabanq.com>
2008-04-01 18:26:47 -0400

    Make WvLog work like in C++ when given partial lines of text.
    Also make WvStream.print() assert if it doesn't manage to write all the
    bytes; we don't want *that* going unnoticed!

--- wvlog.cs
+++ wvlog.cs
@@ -5,6 +5,52 @@ using Wv.Extensions;
 
 namespace Wv
 {
+    public class WvLogConsole
+    {
+	Stream outstr = Console.OpenStandardError();
+	string open_header = null;
+	byte[] nl = "\n".ToUTF8();
+	
+	void w(byte[] b)
+	{
+	    outstr.Write(b, 0, b.Length);
+	}
+	
+	void w(string s)
+	{
+	    w(s.ToUTF8());
+	}
+	
+	public void writelines(string header, WvBuf outbuf)
+	{
+	    // finish previous partial line, if necessary
+	    if (outbuf.used > 0 
+		&& open_header != null && open_header != header)
+	    {
+		w(nl);
+		open_header = null;
+	    }
+	    
+	    uint i;
+	    while ((i = outbuf.strchr('\n')) > 0)
+	    {
+		if (open_header == null)
+		    w(header);
+		w(outbuf.get(i));
+		open_header = null;
+	    }
+	    
+	    // ending partial line
+	    if (outbuf.used > 0)
+	    {
+		if (open_header == null)
+		    w(header);
+		w(outbuf.get(outbuf.used));
+		open_header = header;
+	    }
+	}
+    }
+    
     public class WvLog : WvStream
     {
 	public enum L {
@@ -25,39 +71,50 @@ namespace Wv
 	    get { return _maxlevel; }
 	    set { _maxlevel = value; }
 	}
-	static Stream outstr = Console.OpenStandardError();
+	static WvLogConsole recv = new WvLogConsole();
 	
-	WvBuf outbuf = new WvBuf();
-	byte[] header;
+	string name;
 	L level;
 	
-	private WvLog(byte[] header, L level)
+	string levelstr(L level)
 	{
-	    this.header = header;
-	    this.level = level;
+	    switch (level)
+	    {
+	    case L.Critical: return "!";
+	    case L.Error:    return "E";
+	    case L.Warning:  return "W";
+	    case L.Notice:   return "N";
+	    case L.Info:     return "I";
+	    case L.Debug1:   return "1";
+	    case L.Debug2:   return "2";
+	    case L.Debug3:   return "3";
+	    case L.Debug4:   return "4";
+	    case L.Debug5:   return "5";
+	    default:
+		wv.assert(false, "Unknown loglevel??"); 
+		return "??";
+	    }
 	}
 	
 	public WvLog(string name, L level)
-	    : this(String.Format("<{0}> ", name).ToUTF8(), level)
-	    { }
+	{
+	    this.name = name;
+	    this.level = level;
+	}
 	
 	public WvLog(string name)
 	    : this(name, L.Info)
 	    { }
-
+	
 	public override int write(byte[] buf, int offset, int len)
 	{
 	    if (level > maxlevel)
 		return len; // pretend it's written
 	    
+	    WvBuf outbuf = new WvBuf();
 	    outbuf.put(buf, (uint)offset, (uint)len);
-	    uint i;
-	    while ((i = outbuf.strchr('\n')) > 0)
-	    {
-		outstr.Write(header, 0, header.Length);
-		byte[] b = outbuf.get(i);
-		outstr.Write(b, 0, b.Length);
-	    }
+	    recv.writelines(wv.fmt("{0}<{1}>: ", name, levelstr(level)),
+			    outbuf);
 	    return len;
 	}
 	
@@ -83,7 +140,7 @@ namespace Wv
 
 	public WvLog split(L level)
 	{
-	    return new WvLog(header, level);
+	    return new WvLog(name, level);
 	}
     }
 }
