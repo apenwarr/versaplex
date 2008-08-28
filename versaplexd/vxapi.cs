55a2d8c5dbcc5fd7b5a182622de8f5f0fecc78b6
Lukasz Kosewski <lkosewsk@gmail.com>
2008-08-28 15:46:30 -0400

    "ExecChunkRecordset" call added to Versaplexd.
    Making a big query to Versaplex?  Don't want to kill your DB server
    with storing an entire batch of results in memory at once?
    
    Well, now you don't have to; ExecChunkRecordset is here!  Here's how
    it works:
    - You send Versaplexd an "ExecChunkRecordset" call + query.
    - Versaplexd sends you back X number of roughly 1 MB-large signals.
    (Not exactly, because there isn't really a good way to get the
    marshaled size of a result set, so it's an approximation).  The
    signals have the same data format at a response from ExecRecordset,
    ie. table data + row data + nullity data.
    - Versaplexd sends you a vx.db.nomoredata Error to indicate end of
    data.
    - You party hard.

--- versaplexd/vxapi.cs
+++ versaplexd/vxapi.cs
@@ -32,6 +32,248 @@ internal static class VxDb {
         }
     }
 
+
+    internal static void SendChunkRecordSignal(Message call, object sender,
+					    VxColumnInfo[] colinfo,
+					    object[][] data, byte[][] nulls)
+    {
+	MessageWriter writer =
+	    VxDbInterfaceRouter.PrepareRecordsetWriter(colinfo, data, nulls);
+
+	Message signal = VxDbus.CreateSignal(sender,
+					    "ChunkRecordsetSig",
+					    "a(issnny)vaay",
+					    writer);
+		    
+	// For debugging
+	signal.WriteHeader();
+	VxDbus.MessageDump(" >> ", signal);
+
+	call.Connection.Send(signal);
+    }
+
+
+    internal static void ExecChunkRecordset(Message call, out Message reply)
+    {
+	string connid = VxDbInterfaceRouter.GetClientId(call);
+        if (connid == null)
+        {
+            reply = VxDbus.CreateError(
+                    "org.freedesktop.DBus.Error.Failed",
+                    "Could not identify the client", call);
+            return;
+        }
+        
+	MessageReader reader = new MessageReader(call);
+
+        object oquery;
+        reader.GetValue(typeof(string), out oquery);
+
+	string query = (string)oquery;
+	string iquery = query.ToLower().Trim();
+	reply = null;
+        // XXX this is fishy, really... whitespace fucks it up.
+
+        if (iquery.StartsWith("list tables"))
+            query = "exec sp_tables";
+        else if (iquery.StartsWith("list columns "))
+            query = String.Format("exec sp_columns @table_name='{0}'",
+                    query.Substring(13));
+        else if (iquery.StartsWith("list all table") &&
+                iquery.StartsWith("list all tablefunction") == false)
+            query = "select distinct cast(Name as varchar(max)) Name"
+                + " from sysobjects "
+                + " where objectproperty(id,'IsTable')=1 "
+                + " and xtype='U' "
+                + " order by Name ";
+        else if (iquery.StartsWith("list all"))
+            // Format: list all {view|trigger|procedure|scalarfunction|tablefunction}
+            // Returns: a list of all of whatever
+            query = String.Format(
+                    "select distinct "
+                    + " cast (object_name(id) as varchar(256)) Name "
+                    + " from syscomments "
+                    + " where objectproperty(id,'Is{0}') = 1 "
+                    + " order by Name ",
+                    query.Split(' ')[2].Trim());
+        else if (iquery.StartsWith("get object"))
+            // Format: 
+            // get object {view|trigger|procedure|scalarfunction|tablefunction} name
+            // Returns: the "source code" to the object
+            query = String.Format(
+                    "select cast(text as varchar(max)) text "
+                    + "from syscomments "
+                    + "where objectproperty(id, 'Is{0}') = 1 "
+                    + "and object_name(id) = '{1}' "
+                    + "order by number, colid ",
+                    query.Split(' ')[2].Trim(), 
+                    query.Split(' ')[3].Trim());
+	else if (iquery.StartsWith("drop") || iquery.StartsWith("create") || iquery.StartsWith("insert"))
+	{
+            reply = VxDbus.CreateError(
+                    "vx.db.exception", 
+                    "Don't use ChunkRecordset to modify the DB, silly", call);
+	    // FIXME:  Need a way to handle this?  Abort?
+	    //ExecRecordset(connid, query, out colinfo, out data, out nullity);
+	    return;
+	}
+
+        log.print(WvLog.L.Debug3, "ExecChunkRecordset {0}\n", query);
+
+        SqlConnection conn = null;
+	object sender;
+        if (!call.Header.Fields.TryGetValue(FieldCode.Sender, out sender))
+	    sender = null;
+
+        try
+	{
+            conn = VxSqlPool.TakeConnection(connid);
+
+	    // FIXME:  Sadly, this is stupidly similar to ExecRecordset.
+	    // Anything we can do here to identify commonalities?
+            using (WvDbi dbi = new WvDbi(conn))
+            {
+		List<object[]> rows = new List<object[]>();
+		List<byte[]> rownulls = new List<byte[]>();
+		VxColumnInfo[] colinfo = null;
+		int cursize = 0;  //Our size here is totally an approximation
+                
+		foreach (WvSqlRow cur_row in dbi.select(query))
+                {
+		    if (colinfo == null)
+			colinfo = ProcessChunkSchema(cur_row.schema);
+                    
+		    object[] row = new object[cur_row.schema.DefaultView.Count];
+                    byte[] rownull = new byte[cur_row.schema.DefaultView.Count];
+
+                    for (int i = 0; i < cur_row.schema.DefaultView.Count; i++) 
+                    {
+			WvAutoCast cval = cur_row[i];
+                        bool isnull = cval.IsNull;
+
+                        row[i] = null;
+
+                        rownull[i] = isnull ? (byte)1 : (byte)0;
+			cursize += rownull.Length;
+
+                        switch (colinfo[i].VxColumnType) 
+                        {
+                            case VxColumnType.Int64:
+                                row[i] = !isnull ?
+                                    (Int64)cval : new Int64();
+				cursize += sizeof(Int64);
+                                break;
+                            case VxColumnType.Int32:
+                                row[i] = !isnull ?
+                                    (Int32)cval : new Int32();
+				cursize += sizeof(Int32);
+                                break;
+                            case VxColumnType.Int16:
+                                row[i] = !isnull ?
+                                    (Int16)cval : new Int16();
+				cursize += sizeof(Int16);
+                                break;
+                            case VxColumnType.UInt8:
+                                row[i] = !isnull ?
+                                    (Byte)cval : new Byte();
+				cursize += sizeof(Byte);
+                                break;
+                            case VxColumnType.Bool:
+                                row[i] = !isnull ?
+                                    (bool)cval : new Boolean();
+				cursize += sizeof(Boolean);
+                                break;
+                            case VxColumnType.Double:
+                                // Might return a Single or Double
+                                // FIXME: Check if getting a single causes this
+                                // to croak
+                                row[i] = !isnull ?
+                                    (double)cval : (double)0.0;
+				cursize += sizeof(double);
+                                break;
+                            case VxColumnType.Uuid:
+				//FIXME:  Do I work?
+                                row[i] = !isnull ?
+                                    (string)cval : "";
+				cursize += !isnull ?
+				    ((string)cval).Length * sizeof(Char) : 0;
+                                break;
+                            case VxColumnType.Binary:
+                                {
+                                    if (isnull) 
+                                    {
+                                        row[i] = new byte[0];
+                                        break;
+                                    }
+
+				    row[i] = (byte[])cval;
+				    cursize += ((byte[])cval).Length;
+                                    break;
+                                }
+                            case VxColumnType.String:
+                                row[i] = !isnull ? (string)cval : "";
+				cursize += !isnull ?
+				    ((string)cval).Length * sizeof(Char) : 0;
+                                break;
+                            case VxColumnType.DateTime:
+                                row[i] = !isnull ?
+                                    new VxDbusDateTime((DateTime)cval) :
+                                    new VxDbusDateTime();
+				cursize += System.Runtime.InteropServices.Marshal.SizeOf((DateTime)row[i]);
+                                break;
+                            case VxColumnType.Decimal:
+                                row[i] = !isnull ?
+                                    ((Decimal)cval).ToString() : "";
+				cursize += ((string)(row[i])).Length *
+					    sizeof(Char);
+                                break;
+                        }
+                    }
+
+                    rows.Add(row);
+                    rownulls.Add(rownull);
+		    
+		    if (cursize >= 1024*1024) //approx 1 MB
+		    {
+			log.print(WvLog.L.Debug4, "(1 MB reached; {0} rows)\n",
+						    rows.Count);
+
+			SendChunkRecordSignal(call, sender, colinfo,
+						rows.ToArray(),
+						rownulls.ToArray());
+
+			rows = new List<object[]>();
+			rownulls = new List<byte[]>();
+			cursize = 0;
+		    }
+                }
+
+		if (colinfo == null) // This is gross
+		{
+		    colinfo = ProcessChunkSchema(dbi.statement_schema(query));
+		    cursize = 1;  //HACK:  so that we trigger the code below
+		}
+
+		if (cursize > 0)
+		{
+		    log.print(WvLog.L.Debug4, "(Remaining data; {0} rows)\n",
+					     rows.Count);
+		
+		    SendChunkRecordSignal(call, sender, colinfo,
+						rows.ToArray(),
+						rownulls.ToArray());
+		}
+            }
+	    reply = VxDbus.CreateError("vx.db.nomoredata", 
+					"No more data available.", call);
+        } catch (SqlException e) {
+            throw new VxSqlException(e.Message, e);
+        } finally {
+            if (conn != null)
+                VxSqlPool.ReleaseConnection(conn);
+        }
+    }
+
     internal static void ExecRecordset(string connid, string query,
             out VxColumnInfo[] colinfo, out object[][] data,
             out byte[][] nullity)
@@ -252,6 +494,79 @@ internal static class VxDb {
             }
         }
     }
+
+    private static VxColumnInfo[] ProcessChunkSchema(DataTable schema)
+    {
+	// FIXME:  This is stupidly similar to ProcessSchema
+        VxColumnInfo[] colinfo = new VxColumnInfo[schema.DefaultView.Count];
+
+        if (schema.DefaultView.Count <= 0) 
+            return colinfo;
+
+        int i = 0;
+
+        foreach (DataRowView col in schema.DefaultView)
+	{
+            foreach (DataColumn c in schema.Columns) 
+            {
+                log.print(WvLog.L.Debug4, "{0}:'{1}'  ", 
+                    c.ColumnName, col[c.ColumnName]);
+            }
+            log.print(WvLog.L.Debug4, "\n\n");
+
+            System.Type type = (System.Type)col["DataType"];
+
+            if (type == typeof(object))
+	    {
+                // We're not even going to try to handle this yet
+                throw new VxBadSchemaException("Columns of type sql_variant "
+                    + "are not supported by Versaplex at this time");
+            }
+
+            VxColumnType coltype;
+
+            if (type == typeof(Int64)) {
+                coltype = VxColumnType.Int64;
+            } else if (type == typeof(Int32)) {
+                coltype = VxColumnType.Int32;
+            } else if (type == typeof(Int16)) {
+                coltype = VxColumnType.Int16;
+            } else if (type == typeof(Byte)) {
+                coltype = VxColumnType.UInt8;
+            } else if (type == typeof(Boolean)) {
+                coltype = VxColumnType.Bool;
+            } else if (type == typeof(Single) || type == typeof(Double)) {
+                coltype = VxColumnType.Double;
+            } else if (type == typeof(Guid)) {
+                coltype = VxColumnType.Uuid;
+            } else if (type == typeof(Byte[])) {
+                coltype = VxColumnType.Binary;
+            } else if (type == typeof(string)) {
+                coltype = VxColumnType.String;
+            } else if (type == typeof(DateTime)) {
+                coltype = VxColumnType.DateTime;
+            } else if (type == typeof(Decimal)) {
+                coltype = VxColumnType.Decimal;
+            } else {
+                throw new VxBadSchemaException("Columns of type "
+                        + type.ToString() + " are not supported by "
+                        + "Versaplex at this time " +
+                        "(column " + col["ColumnName"].ToString() + ")");
+            }
+
+            bool isnull = (bool)col["AllowDBNull"];
+            int size = (int)col["ColumnSize"];
+            short precision = (short)col["NumericPrecision"];
+            short scale = (short)col["NumericScale"];
+
+            colinfo[i] = new VxColumnInfo(col["ColumnName"].ToString(),
+                    coltype, isnull, size, precision, scale);
+
+            i++;
+        }
+
+	return colinfo;
+    }
 }
 
 public class VxDbInterfaceRouter : VxInterfaceRouter 
@@ -273,6 +588,7 @@ public class VxDbInterfaceRouter : VxInterfaceRouter
         methods.Add("Quit", CallQuit);
         methods.Add("ExecScalar", CallExecScalar);
         methods.Add("ExecRecordset", CallExecRecordset);
+        methods.Add("ExecChunkRecordset", CallExecChunkRecordset);
         methods.Add("GetSchemaChecksums", CallGetSchemaChecksums);
         methods.Add("GetSchema", CallGetSchema);
         methods.Add("PutSchema", CallPutSchema);
@@ -392,16 +708,7 @@ public class VxDbInterfaceRouter : VxInterfaceRouter
             out colinfo, out data, out nullity);
 
         // FIXME: Add vx.db.toomuchdata error
-        MessageWriter writer =
-                new MessageWriter(Connection.NativeEndianness);
-
-	WriteColInfo(writer, colinfo);
-	writer.Write(typeof(Signature), VxColumnInfoToArraySignature(colinfo));
-	writer.WriteDelegatePrependSize(delegate(MessageWriter w) 
-	    {
-		WriteStructArray(w, VxColumnInfoToType(colinfo), data);
-	    }, 8);
-	writer.Write(typeof(byte[][]), nullity);
+	MessageWriter writer = PrepareRecordsetWriter(colinfo, data, nullity);
 	
         reply = VxDbus.CreateReply(call, "a(issnny)vaay", writer);
     }
@@ -506,29 +813,35 @@ public class VxDbInterfaceRouter : VxInterfaceRouter
             out colinfo, out data, out nullity);
 
         // FIXME: Add vx.db.toomuchdata error
-        MessageWriter writer =
-                new MessageWriter(Connection.NativeEndianness);
-
-	WriteColInfo(writer, colinfo);
-	if (colinfo.Length <= 0) {
-	    // Some clients can't parse a() (empty struct) properly, so
-	    // we'll have an empty array of (i) instead.
-	    writer.Write(typeof(Signature), new Signature("a(i)"));
-	} else {
-	    writer.Write(typeof(Signature), 
-			 VxColumnInfoToArraySignature(colinfo));
-	}
-	writer.WriteDelegatePrependSize(delegate(MessageWriter w)
-	    {
-		WriteStructArray(w, VxColumnInfoToType(colinfo), data);
-	    }, 8);
-	writer.Write(typeof(byte[][]), nullity);
+	MessageWriter writer = PrepareRecordsetWriter(colinfo, data, nullity);
 	
         reply = VxDbus.CreateReply(call, "a(issnny)vaay", writer);
 
         // For debugging
         reply.WriteHeader();
         VxDbus.MessageDump(" >> ", reply);
+    }
+
+    private static void CallExecChunkRecordset(Message call, out Message reply)
+    {
+	// XXX: Stuff in this comment block shamelessly stolen from
+	// "CallExecRecordset".
+        if (call.Signature.ToString() != "s") {
+            reply = CreateUnknownMethodReply(call, "ExecChunkRecordset");
+            return;
+        }
+
+        if (call.Body == null) {
+            reply = VxDbus.CreateError(
+                    "org.freedesktop.DBus.Error.InvalidSignature",
+                    "Signature provided but no body received", call);
+            return;
+        }
+	/// XXX
+
+        VxDb.ExecChunkRecordset(call, out reply);
+
+        //reply = VxDbus.CreateReply(call, "a(issnny)vaay", writer);
     }
 
     private static Signature VxColumnInfoToArraySignature(VxColumnInfo[] vxci)
@@ -861,6 +1174,30 @@ public class VxDbInterfaceRouter : VxInterfaceRouter
         }
 
         reply = VxDbus.CreateReply(call);
+    }
+
+    public static MessageWriter PrepareRecordsetWriter(VxColumnInfo[] colinfo,
+						object[][] data,
+						byte[][] nulldata)
+    {
+	MessageWriter writer = new MessageWriter(Connection.NativeEndianness);
+
+	WriteColInfo(writer, colinfo);
+	if (colinfo.Length <= 0)
+	{
+	    // Some clients can't parse a() (empty struct) properly, so
+	    // we'll have an empty array of (i) instead.
+	    writer.Write(typeof(Signature), new Signature("a(i)"));
+	}
+	else
+	    writer.Write(typeof(Signature), VxColumnInfoToArraySignature(colinfo));
+	writer.WriteDelegatePrependSize(delegate(MessageWriter w)
+		{
+		    WriteStructArray(w, VxColumnInfoToType(colinfo), data);
+		}, 8);
+	writer.Write(typeof(byte[][]), nulldata);
+
+	return writer;
     }
 }
 
