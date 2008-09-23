f82f792e57b9a762b17d02470fc730ff3011098a
Peter McCurdy <petermccurdy@alumni.uwaterloo.ca>
2008-09-23 15:14:40 -0400

    Rename some schema and variables in putschema.t.cs.
    Also let tests pass in the options to PutSchema instead of having hardcoded
    defaults.

--- versaplexd/t/putschema.t.cs
+++ versaplexd/t/putschema.t.cs
@@ -35,10 +35,16 @@ class PutSchemaTests : SchemamaticTester
 
     public void TestTableUpdate(string tabname, string tabschema)
     {
+        TestTableUpdate(tabname, tabschema, VxPutOpts.None);
+    }
+
+    public void TestTableUpdate(string tabname, string tabschema, 
+        VxPutOpts opts)
+    {
         VxSchema schema = new VxSchema();
         schema.Add("Table", tabname, tabschema, false);
 
-        VxSchemaErrors errs = dbus.Put(schema, null, VxPutOpts.None);
+        VxSchemaErrors errs = dbus.Put(schema, null, opts);
 
         log.print("Received errors: {0}\n", errs.ToString());
         WVPASSEQ(errs.Count, 0);
@@ -51,11 +57,17 @@ class PutSchemaTests : SchemamaticTester
     public void TestTableUpdateError(string tabname, string tabschema, 
         string errmsg, string oldval)
     {
+        TestTableUpdateError(tabname, tabschema, errmsg, oldval, VxPutOpts.None);
+    }
+
+    public void TestTableUpdateError(string tabname, string tabschema, 
+        string errmsg, string oldval, VxPutOpts opts)
+    {
         string key = "Table/" + tabname;
         VxSchema schema = new VxSchema();
         schema.Add("Table", tabname, tabschema, false);
 
-        VxSchemaErrors errs = dbus.Put(schema, null, VxPutOpts.None);
+        VxSchemaErrors errs = dbus.Put(schema, null, opts);
         log.print("Received errors: {0}\n", errs.ToString());
 
         WVPASSEQ(errs.Count, 1);
@@ -72,129 +84,129 @@ class PutSchemaTests : SchemamaticTester
     [Test, Category("Schemamatic"), Category("PutSchema")]
     public void TestChangingIndexes()
     {
-        try { VxExec("drop table Tab1"); } catch { }
+        try { VxExec("drop table TestTable"); } catch { }
 
         WVPASS(1);
-        string tab1sch = "column: name=f1,type=int,null=0\n" + 
+        string testschema = "column: name=f1,type=int,null=0\n" + 
                 "column: name=f2,type=money,null=0\n" + 
                 "column: name=f3,type=varchar,null=1,length=80\n" +
                 "index: column=f2,column=f3 DESC,name=Idx1,unique=1,clustered=2\n" +
                 "primary-key: column=f1,column=f2,clustered=1\n";
-        TestTableUpdate("Tab1", tab1sch);
+        TestTableUpdate("TestTable", testschema);
 
         // Change the index: line slightly.
         WVPASS(2);
-        string tab1sch2 = "column: name=f1,type=int,null=0\n" + 
+        string testschema2 = "column: name=f1,type=int,null=0\n" + 
                 "column: name=f2,type=money,null=0\n" + 
                 "column: name=f3,type=varchar,null=1,length=80\n" +
                 "index: column=f1,column=f3 DESC,name=Idx1,unique=1,clustered=2\n" +
                 "primary-key: column=f1,column=f2,clustered=1\n";
-        TestTableUpdate("Tab1", tab1sch2);
+        TestTableUpdate("TestTable", testschema2);
 
         // No index at all
         WVPASS(3);
-        string tab1sch3 = "column: name=f1,type=int,null=0\n" + 
+        string testschema3 = "column: name=f1,type=int,null=0\n" + 
                 "column: name=f2,type=money,null=0\n" + 
                 "column: name=f3,type=varchar,null=1,length=80\n" +
                 "primary-key: column=f1,column=f2,clustered=1\n";
-        TestTableUpdate("Tab1", tab1sch3);
+        TestTableUpdate("TestTable", testschema3);
 
         // Add the index back, and another for good measure
         WVPASS(4);
-        string tab1sch4 = "column: name=f1,type=int,null=0\n" + 
+        string testschema4 = "column: name=f1,type=int,null=0\n" + 
                 "column: name=f2,type=money,null=0\n" + 
                 "column: name=f3,type=varchar,null=1,length=80\n" +
                 "index: column=f1,column=f3 DESC,name=Idx1,unique=1,clustered=2\n" +
                 "index: column=f2,column=f3,name=Idx2\n" +
                 "primary-key: column=f1,column=f2,clustered=1\n";
         VxSchema schema = new VxSchema();
-        schema.Add("Table", "Tab1", tab1sch4, false);
+        schema.Add("Table", "TestTable", testschema4, false);
 
         VxSchemaErrors errs = dbus.Put(schema, null, VxPutOpts.None);
         log.print("Received errors: {0}\n", errs.ToString());
         WVPASSEQ(errs.Count, 0);
 
         // Check that we get the default unique=0 and clustered=2 parameters.
-        tab1sch4 = tab1sch4.Replace("Idx2", "Idx2,unique=0,clustered=2");
+        testschema4 = testschema4.Replace("Idx2", "Idx2,unique=0,clustered=2");
 
-        schema = dbus.Get("Table/Tab1");
+        schema = dbus.Get("Table/TestTable");
         WVPASSEQ(schema.Count, 1);
-        CheckTable(schema, "Tab1", tab1sch4);
+        CheckTable(schema, "TestTable", testschema4);
 
         // Check that duplicate index names give errors.
         WVPASS(5);
-        string tab1sch5 = "column: name=f1,type=int,null=0\n" + 
+        string testschema5 = "column: name=f1,type=int,null=0\n" + 
                 "column: name=f2,type=money,null=0\n" + 
                 "column: name=f3,type=varchar,null=1,length=80\n" +
                 "index: column=f1,column=f3 DESC,name=Idx1,unique=1,clustered=2\n" +
                 "index: column=f2,column=f3,name=Idx1,unique=1,clustered=2\n" +
                 "primary-key: column=f1,column=f2,clustered=1\n";
         string errmsg = "Duplicate table entry 'index: Idx1' found.";
-        TestTableUpdateError("Tab1", tab1sch5, errmsg, tab1sch4);
+        TestTableUpdateError("TestTable", testschema5, errmsg, testschema4);
 
         // Try renaming an index.
         // Note that indexes are returned alphabetically, and the default name
         // for a primary key is PK_TableName, so the renamed index will show 
         // up after the primary key.
         WVPASS(6);
-        string tab1sch6 = "column: name=f1,type=int,null=0\n" + 
+        string testschema6 = "column: name=f1,type=int,null=0\n" + 
                 "column: name=f2,type=money,null=0\n" + 
                 "column: name=f3,type=varchar,null=1,length=80\n" +
                 "primary-key: column=f1,column=f2,clustered=1\n" + 
                 "index: column=f1,column=f3 DESC,name=RenamedIndex," + 
                     "unique=1,clustered=2\n";
-        TestTableUpdate("Tab1", tab1sch6);
+        TestTableUpdate("TestTable", testschema6);
 
-        try { VxExec("drop table Tab1"); } catch { }
+        try { VxExec("drop table TestTable"); } catch { }
     }
 
     [Test, Category("Schemamatic"), Category("PutSchema")]
     public void TestChangingPrimaryKeys()
     {
-        try { VxExec("drop table Tab1"); } catch { }
+        try { VxExec("drop table TestTable"); } catch { }
 
         WVPASS(1);
-        string tab1sch = "column: name=f1,type=int,null=0\n" + 
+        string testschema = "column: name=f1,type=int,null=0\n" + 
                 "column: name=f2,type=money,null=0\n" + 
                 "column: name=f3,type=varchar,null=1,length=80\n" +
                 "index: column=f2,column=f3 DESC,name=Idx1,unique=1,clustered=2\n" +
                 "primary-key: column=f1,column=f2,clustered=1\n";
-        TestTableUpdate("Tab1", tab1sch);
+        TestTableUpdate("TestTable", testschema);
 
         // Change the primary-key: line: try specifying a name, changing the
         // columns, and omitting the optional "clustered" parameter.
         WVPASS(2);
-        string tab1sch2 = "column: name=f1,type=int,null=0\n" + 
+        string testschema2 = "column: name=f1,type=int,null=0\n" + 
                 "column: name=f2,type=money,null=0\n" + 
                 "column: name=f3,type=varchar,null=1,length=80\n" +
                 "index: column=f1,column=f3 DESC,name=Idx1,unique=1,clustered=2\n" +
                 "primary-key: name=TestPK,column=f1\n";
         VxSchema schema = new VxSchema();
-        schema.Add("Table", "Tab1", tab1sch2, false);
+        schema.Add("Table", "TestTable", testschema2, false);
         VxSchemaErrors errs = dbus.Put(schema, null, VxPutOpts.None);
 
         // We'll get the default clustered value added back
-        tab1sch2 = tab1sch2.Replace("primary-key: name=TestPK,column=f1", 
+        testschema2 = testschema2.Replace("primary-key: name=TestPK,column=f1", 
             "primary-key: name=TestPK,column=f1,clustered=2");
 
         log.print("Received errors: {0}\n", errs.ToString());
         WVPASSEQ(errs.Count, 0);
 
-        schema = dbus.Get("Table/Tab1");
+        schema = dbus.Get("Table/TestTable");
         WVPASSEQ(schema.Count, 1);
-        CheckTable(schema, "Tab1", tab1sch2);
+        CheckTable(schema, "TestTable", testschema2);
 
         // Remove the primary-key: line.
         WVPASS(3);
-        string tab1sch3 = "column: name=f1,type=int,null=0\n" + 
+        string testschema3 = "column: name=f1,type=int,null=0\n" + 
                 "column: name=f2,type=money,null=0\n" + 
                 "column: name=f3,type=varchar,null=1,length=80\n" +
                 "index: column=f1,column=f3 DESC,name=Idx1,unique=1,clustered=2\n";
-        TestTableUpdate("Tab1", tab1sch3);
+        TestTableUpdate("TestTable", testschema3);
 
         // Try to add two primary keys
         WVPASS(4);
-        string tab1sch4 = "column: name=f1,type=int,null=0\n" + 
+        string testschema4 = "column: name=f1,type=int,null=0\n" + 
                 "column: name=f2,type=money,null=0\n" + 
                 "column: name=f3,type=varchar,null=1,length=80\n" +
                 "index: column=f1,column=f3 DESC,name=Idx1,unique=1,clustered=2\n" + 
@@ -202,9 +214,9 @@ class PutSchemaTests : SchemamaticTester
                 "primary-key: column=f2,clustered=1\n";
         string errmsg = "Duplicate table entry 'primary-key' found.";
 
-        TestTableUpdateError("Tab1", tab1sch4, errmsg, tab1sch3);
+        TestTableUpdateError("TestTable", testschema4, errmsg, testschema3);
 
-        try { VxExec("drop table Tab1"); } catch { }
+        try { VxExec("drop table TestTable"); } catch { }
     }
 
     [Test, Category("Schemamatic"), Category("PutSchema")]
