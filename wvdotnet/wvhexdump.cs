2a0affde0ba62da596d113a36c39231d69572792
Peter McCurdy <petermccurdy@alumni.uwaterloo.ca>
2008-09-24 17:31:28 -0400

    Use StringBuilder instead of WvBuf to build the string in hexdump.
    StringBuilder is much, much faster than WvBuf when you're adding a lot of
    small strings.  With this change, I can now hexdump 100 MB of data in the
    time it used to take for 1 MB.
    
    Also don't call wv.fmt unnecessarily, when it was just used to tack on a
    space.

--- wvdotnet/wvhexdump.cs
+++ wvdotnet/wvhexdump.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using System.Linq;
 
 namespace Wv
@@ -23,7 +24,7 @@ namespace Wv
 	public static string _hexdump(byte[] data,
 				      int startoffset, int maxlen)
 	{
-	    WvBuf buf = new WvBuf();
+            var sb = new StringBuilder();
 	    
 	    // This is overly complicated so that the body and header of
 	    // the same buffer can be printed separately yet still show the
@@ -33,52 +34,65 @@ namespace Wv
 	    int rowoffset = startoffset & (~0xf);
 	    int coloffset = startoffset & 0xf;
 	    
+            // 16 bytes of input turns into 70 bytes of output, plus newline, 
+            int linelen = 71;
+            // If we have to deal with more than 2^16 bytes of data, the
+            // leading count number will take more space
+            if (length > 1<<16)
+                linelen += 4;
+
+            // Add an extra line since we might have partial lines or such.
+            int result_size_est = ((length / 16) + 1) * linelen; 
+            // Note: it's important to set the right capacity when dealing 
+            // with large quantities of data.
+            sb.EnsureCapacity(result_size_est);
+
 	    int cnt = rowoffset;
 	    for (int i=0; i < length; cnt += 16) {
-		buf.put("{0} ", cnt.ToString("x4"));
+		sb.Append(cnt.ToString("x4")).Append(" ");
 		
 		int co=0;
 		if (coloffset > 0 && i == 0) {
 		    for (int j=0; j < coloffset; j++)
-			buf.put("   ");
+			sb.Append("   ");
 		    
 		    co=coloffset;
 		}
 		
 		// Print out the hex digits
 		for (int j=0; j < 8-co && i+j < length; j++)
-		    buf.put("{0} ", data[i+j].ToString("x2"));
+		    sb.Append(data[i+j].ToString("x2")).Append(" ");
 		
-		buf.put(" ");
+		sb.Append(" ");
 		
 		for (int j=8-co; j < 16-co && i+j < length; j++)
-		    buf.put("{0} ", data[i+j].ToString("x2"));
+		    sb.Append(data[i+j].ToString("x2")).Append(" ");
 		
 		// extra space if incomplete line
 		if (i + 16-co > length) {
 		    for (int j = length - i; j < 16-co; j++)
-			buf.put("   ");
+			sb.Append("   ");
 		}
 		
 		if (co > 0) {
 		    for (int j=0; j < co; j++)
-			buf.put(" ");
+			sb.Append(" ");
 		}
 		
 		for (int j=0; j < 16-co && i+j < length; j++) {
 		    if (31 < data[i+j] && data[i+j] < 127) {
-			buf.put((char)data[i+j]);
+			sb.Append((char)data[i+j]);
 		    } else {
-			buf.put('.');
+			sb.Append('.');
 		    }
 		}
 		
-		buf.put("\n");
+		sb.Append("\n");
 		
 		i += 16-co;
 	    }
 	
-	    return buf.getstr();
+	    return sb.ToString();
 	}
 	
 	public static object hexdump(byte[] data,
