c1ae92492e552c1e8072a2612188b0f8486ee524
Avery Pennarun <apenwarr@gmail.com>
2009-04-15 14:11:41 -0400

    Remove stuff that's not needed to build wvdbus-sharp.

--- examples/delphi-demo/Schema/DATA/00100-DemoTable.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM [DemoTable];
-SET IDENTITY_INSERT [DemoTable] ON;
-INSERT INTO DemoTable ([Id],[LastName],[FirstName],[Puppies],[LastUpdated]) VALUES (1,'Pennarun','Avery',13,'2008-11-12 21:52:41.280');
-INSERT INTO DemoTable ([Id],[LastName],[FirstName],[Puppies],[LastUpdated]) VALUES (2,'Smith','Bob',99,'2008-11-12 21:52:53.090');
-INSERT INTO DemoTable ([Id],[LastName],[FirstName],[Puppies],[LastUpdated]) VALUES (3,'Looooooooooooooooooongname','Bill "The Fish"',122,'2008-11-12 21:53:35.673');
-SET IDENTITY_INSERT [DemoTable] OFF;
--- vxodbc/linker-test.cc
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("If you can see this, your shared library works.\n");
-    return 0;
-}
